6fa16fee869d15867dc13333df37f05f913bb320
Jenil Gajera <jenil.gajera@shunyavkash.com>
2026-01-21 12:59:52 +0530

    Fix tsconfig.json

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import app from './app'; 
+import app from './app.ts'; 
 
 describe('GET /', () => {
   it('should return hello message', async () => {
--- src/config/vitest.config.ts
+++ src/config/vitest.config.ts
@@ -1,11 +1,12 @@
+/// <reference types="vitest/globals" />
+
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    globals: true,  // Yeh line sabse important – describe, it, expect global ban jaayenge
-    environment: 'node',  // Backend Node.js ke liye
-    include: ['src/**/*.test.ts'],  // Tumhare test files ko pick karega
-    // Optional: agar chahiye toh
-    // setupFiles: ['./src/setupTests.ts'], // baad mein agar extra setup chahiye
+    globals: true,  
+    environment: 'node',  
+    include: ['src/**/*.test.ts'],  
+   
   },
 });
